a4edc5d14914ecbcad82397aac6efe513ff480c7
rmontoza <rodrigo.montoza@gmail.com>
2024-09-22 20:15:13 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,96 @@
+# Provider AWS
 provider "aws" {
-  region = "us-east-1" # Defina a região
+  region = "us-east-1"  # Ajuste para a sua região
 }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.eks_auth.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.eks_auth.token
+# Criar VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
   }
 }
 
-data "aws_eks_cluster" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-data "aws_eks_cluster_auth" "eks_auth" {
-  name = aws_eks_cluster.eks_cluster.name
-}
-
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "lanhonete-eks-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  version = "1.29"  # Especificar a versão do Kubernetes que você deseja usar
-
-  vpc_config {
-    subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+# Criar Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "eks-igw"
   }
 }
 
+# Criar Tabela de Rotas Pública
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "eks-public-route-table"
+  }
+}
+
+# Criar Subnets Públicas
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-public-subnet-b"
+  }
+}
+
+# Associar Tabela de Rotas com Subnets Públicas
+resource "aws_route_table_association" "public_a_assoc" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_b_assoc" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Criar o Security Group para o EKS
+resource "aws_security_group" "eks_security_group" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-security-group"
+  }
+}
+
+# Criar Role e Policies para o EKS Cluster
 resource "aws_iam_role" "eks_role" {
-  name = "lanchonete_eks_role"
+  name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -48,50 +102,73 @@ resource "aws_iam_role" "eks_role" {
       }
     }]
   })
+
+  tags = {
+    Name = "eks-role"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_role_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "eks_vpc_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+# Criar o Cluster EKS
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "lanchonete-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+  version  = "1.29"  # Definir a versão do Kubernetes
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    security_group_ids = [aws_security_group.eks_security_group.id]
+  }
+
+  tags = {
+    Name = "eks-cluster"
+  }
 }
 
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "172.31.0.0/16"
+# Criar Role para os Nodes do EKS
+resource "aws_iam_role" "eks_node_role" {
+  name = "lanchonete-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "eks-node-role"
+  }
 }
 
-resource "aws_subnet" "public_a" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "172.31.0.0/20"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true # Garantir que IPs públicos sejam atribuídos
+# Anexar políticas necessárias para os Nodes
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_subnet" "public_b" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "172.31.80.0/20"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true # Garantir que IPs públicos sejam atribuídos
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-
-resource "aws_route_table_association" "public_a_assoc" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_route_table_association" "public_b_assoc" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
+# Criar o Node Group para o Cluster EKS
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "lanchonete-node-group"
+  node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 
@@ -100,34 +177,10 @@ resource "aws_eks_node_group" "node_group" {
     max_size     = 3
     min_size     = 1
   }
-}
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "lanchonete_eks_node_role"
+  ami_type = "AL2_x86_64"  # Usando Amazon Linux 2 como AMI para os Nodes
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "cni_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "registry_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  tags = {
+    Name = "eks-node-group"
+  }
 }
