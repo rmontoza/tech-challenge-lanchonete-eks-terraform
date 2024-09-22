63aa53e1a69adee608d531c0740bc58555bc92b1
rmontoza <rodrigo.montoza@gmail.com>
2024-09-22 19:10:22 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,18 +59,18 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_attachment" {
 }
 
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.0.0/16"
 }
 
 resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "172.31.0.0/20"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "public_b" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "172.31.80.0/20"
   availability_zone = "us-east-1b"
 }
 
